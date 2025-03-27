38a0cc5aec33b69de5a4e83539f92fdea1fc7c16
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-03-27 08:55:41 -0400

    Updates

---
+++ my-app/src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import './css/Navigation.css';
+
+function Navigation() {
+    return (
+        <nav id="main-nav">
+            <ul>
+                <li>HELLO</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
