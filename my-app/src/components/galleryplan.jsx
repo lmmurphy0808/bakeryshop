762d488db84747516ecc5b52b839ad0257c49851
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-04-26 02:57:04 -0400

    edits

--- my-app/src/components/galleryplan.jsx
+++ my-app/src/components/galleryplan.jsx
@@ -1,12 +1,25 @@
 import "../styles/galleryplan.css";
+import React from "react";
 
 const Gallery = (cookies) => {
     //const imageSrc = 'https://lmmurphy0808.github.io/json/images/cookies/${cookies.main_image}';
     //const imageSrc = `http://localhost:3001/${cookies.main_image}`;
-    //http://localhost:3001/
-    const imageSrc = `https://bakery-backend-t744.onrender.com/${cookies.main_image}`;
+    const imageSrc = 'http://localhost:3001/';
+    //const imageSrc = `https://bakery-backend-t744.onrender.com/${cookies.main_image}`;
     return (
-        <section className="Gallery"></section>
+        <div>
+            <section className="Gallery">
+                <section className="feature-image">
+                    <img src={imageSrc} alt={cookiePlain.name} />
+                </section>
+                <section className="info">
+                    <h2>{cookie.name}</h2>
+
+                    <p>Level: {cookie.level}</p>
+                    <p>Rating: {cookie.rating}</p>
+                </section>
+            </section>
+        </div>
     )
 };  
 
